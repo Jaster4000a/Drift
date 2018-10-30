6a2ffcf3b4fbd5f7f29ea5e494b4ff29ec98ad99
Jaster4000a <jasoncabrejos@hotmail.com>
2018-10-30 18:14:42 -0500

    Add files via upload

---
+++ LIGHT/LIGHT.ino
@@ -0,0 +1,44 @@
+
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+#define PIN            6
+
+#define NUMPIXELS      30
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+int delayval = 5;
+
+void setup() {
+#if defined (__AVR_ATtiny85__)
+  if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
+#endif
+
+  pixels.begin(); 
+}
+
+void loop() {
+
+  for(int j=0;j<256;j++){
+    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+    for(int i=0;i<NUMPIXELS;i++){
+    pixels.setPixelColor(i, pixels.Color(j,0,0));
+    pixels.show(); // This sends the updated pixel color to the hardware.
+    delay(10);
+    }
+    for(int i=0;i<NUMPIXELS;i++){
+    pixels.setPixelColor(i, pixels.Color(0,j,0));
+    pixels.show(); // This sends the updated pixel color to the hardware.
+    delay(10);
+    }
+    for(int i=0;i<NUMPIXELS;i++){
+    pixels.setPixelColor(i, pixels.Color(0,0,j));
+    pixels.show(); // This sends the updated pixel color to the hardware.
+    delay(10);
+    }
+    delay(10);
+  }
+}
---
+++ ultra_sonic/ultra_sonic.ino
@@ -0,0 +1,124 @@
+// Includes the Servo library
+#include<SoftwareSerial.h>
+#include <Servo.h>
+// Defines Tirg and Echo pins of the Ultrasonic Sensor
+const int trigPin = 4;
+const int echoPin = 5;
+int enA = 9;
+int in1 = 11;
+int in2 = 10;
+int z1 = 0;
+int z2 = 0;
+int z3 = 0;
+int target;
+// Variables for the duration and the distance
+long duration;
+int distance;
+Servo myServo; // Creates a servo object for controlling the servo motor
+Servo steer;
+
+// Function for calculating the distance measured by the Ultrasonic sensor
+int calculateDistance() {
+
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH); 
+  distance = duration * 0.034 / 2;
+  return distance;
+}
+
+void scan() {
+  for (int i = 150; i > 110; i--) {
+    myServo.write(i);
+    delay(30);
+    distance = calculateDistance();
+    z1 = distance + z1;
+    Serial.print("z1 cm: ");
+    Serial.print(distance);
+    Serial.print(" angle: ");
+    Serial.println(i);
+  }
+  z1 = z1 / 40;
+  for (int i = 110; i > 70; i--) {
+    myServo.write(i);
+    delay(30);
+    distance = calculateDistance();
+    z2 = distance + z2;
+    Serial.print("z2 cm: ");
+    Serial.print(distance);
+    Serial.print(" angle: ");
+    Serial.println(i);
+  }
+  z2 = z2 / 40;
+  for (int i = 70; i > 30; i--) {
+    myServo.write(i);
+    delay(30);
+    distance = calculateDistance();
+    z3 = distance + z3;
+    Serial.print("z3 cm: ");
+    Serial.print(distance);
+    Serial.print(" angle: ");
+    Serial.println(i);
+  }
+  z3 = z3 / 40;
+
+  Serial.print("z1: ");
+  Serial.print(z1);
+  Serial.print(" z2: ");
+  Serial.print(z2);
+  Serial.print(" z3: ");
+  Serial.println(z3);
+}
+
+
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+  myServo.attach(12);
+  steer.attach(13);
+}
+void loop() {
+
+  if (calculateDistance() <= 5 * 2.54){
+  digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
+    analogWrite(enA, 200);
+    delay(2000);
+    digitalWrite(in1, LOW);
+      digitalWrite(in2, LOW);
+  } else if (calculateDistance() <= 20 * 2.54) {
+      digitalWrite(in1, LOW);
+      digitalWrite(in2, LOW);
+      scan();
+      myServo.write(90);
+      target = max(z1, max(z2, z3));
+      Serial.print(target);
+      if (target == z3) {
+        Serial.println(" z3 ");
+        steer.write(130);
+      } else if (target == z1) {
+        Serial.println(" z1 ");
+        steer.write(50);
+      } else if (target == z2) {
+        Serial.println(" z2 ");
+        steer.write(90);
+      }
+      digitalWrite(in1, HIGH);
+      digitalWrite(in2, LOW);
+      analogWrite(enA, 200);
+      delay(5000);
+    } else {
+      steer.write(90);
+      myServo.write(90);
+      digitalWrite(in1, HIGH);
+      digitalWrite(in2, LOW);
+      analogWrite(enA, 200);
+    }
+  }
+
+
